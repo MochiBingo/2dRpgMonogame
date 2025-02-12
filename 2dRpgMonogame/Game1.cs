ed6592be596bc8f339dec82e0474127ef9a64ab5
MochiBingo <mochibingoshep@gmail.com>
2025-02-12 09:39:02 -0400

    added some stuff

--- 2dRpgMonogame/Game1.cs
+++ 2dRpgMonogame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace _2dRpgMonogame
 {
@@ -12,16 +13,12 @@ namespace _2dRpgMonogame
         private int sizeY = 10;
         private bool canWalk;
         private bool isExit;
+        public Random rand = new Random();
 
-        private Texture mapBorder;
-        private Texture ground;
-        private Texture tree;
+        private Texture2D ground;
+        private Texture2D tree;
+        private Texture2D playerSprite;
 
-
-
-        /// <summary>
-        /// this is a summary
-        /// </summary>
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -29,6 +26,10 @@ namespace _2dRpgMonogame
             IsMouseVisible = true;
         }
 
+        protected void GenerateMap()
+        {
+
+        }
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -39,18 +40,17 @@ namespace _2dRpgMonogame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            ground = this.Content.Load<Texture2D>("tile_0000");
+            tree = this.Content.Load<Texture2D>("tile_0005");
+            playerSprite = this.Content.Load<Texture2D>("tile_0094");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            float deltaTime = gameTime.ElapsedGameTime.Milliseconds * 0.01f;
 
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
+            player.Update(deltaTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -60,6 +60,17 @@ namespace _2dRpgMonogame
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+    }
+    public class player
+    {
+        public player(Game1 game, Vector2 initialposition)
+        {
+
+        }
+        public void Update(float deltaTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
         }
     }
 }
--- 2dRpgMonogame/obj/Debug/net6.0/2dRpgMonogame.AssemblyInfo.cs
+++ 2dRpgMonogame/obj/Debug/net6.0/2dRpgMonogame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2dRpgMonogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98cc856b4b01a27c0ec780f2927fc70089a5d135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc17ec4d367c4993d569ea4427486392f47cf69b")]
 [assembly: System.Reflection.AssemblyProductAttribute("2dRpgMonogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2dRpgMonogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
